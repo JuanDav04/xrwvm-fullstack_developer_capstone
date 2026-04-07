0879e4a75ea34237b4c01dd1906103d040b69190
Juandav <136283645+JuanDav04@users.noreply.github.com>
2026-04-07 09:38:12 -0500

    Fix formatting and import statements in restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,17 +1,17 @@
 # Uncomment the imports below before you add the function code
-import requests;
-import os;
-from dotenv import load_dotenv;
+import requests
+import os
+from dotenv import load_dotenv
 
 
 load_dotenv()
 
 
-backend_url = os.getenv(
-    'backend_url', default = "http://localhost:3030")
-sentiment_analyzer_url = os.getenv(
+backend_url=os.getenv(
+    'backend_url', default="http://localhost:3030")
+sentiment_analyzer_url=os.getenv(
     'sentiment_analyzer_url',
-    default = "http://localhost:5050/")
+    default="http://localhost:5050/")
 
 
 def get_request(endpoint, **kwargs):
@@ -47,7 +47,7 @@ def analyze_review_sentiments(text):
 def post_review(data_dict):
     request_url = backend_url + "/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except:
