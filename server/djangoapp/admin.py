be3e3560958e8d6b48facc1afcf434c7c7e33a29
Juandav <136283645+JuanDav04@users.noreply.github.com>
2026-04-07 09:04:20 -0500

    Add CarMakeAdmin and CarModelAdmin to admin site

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -5,12 +5,15 @@ class CarModelInline(admin.TabularInline):
     model = CarModel
     extra = 1
 
+
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'car_make', 'dealer_id', 'car_type', 'year')
+
 
 class CarMakeAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
     inlines = [CarModelInline]
+
 
 admin.site.register(CarMake, CarMakeAdmin)
 admin.site.register(CarModel, CarModelAdmin)
