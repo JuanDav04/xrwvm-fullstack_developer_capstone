e84f61f3b37bcf606b7a4e3d2c261714645d15aa
Juandav <136283645+JuanDav04@users.noreply.github.com>
2026-04-07 09:23:28 -0500

    Remove unnecessary semicolons in admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,5 +1,6 @@
-from django.contrib import admin;
-from .models import CarMake, CarModel;
+from django.contrib import admin
+from .models import CarMake, CarModel
+
 
 class CarModelInline(admin.TabularInline):
     model = CarModel
